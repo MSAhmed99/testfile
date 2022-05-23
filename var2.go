b7e8e349944a8328fb1e162e0fabb5ad9944009c
yankils <msa.shakeel97@gmail.com>
2022-05-23 13:40:51 +0530

    ababa

---
+++ var2.go
@@ -0,0 +1,20 @@
+// Here package keyword is used to
+package main
+
+import "fmt"
+
+func main() {
+
+	// Here, var keyword is used
+	// to create variables
+	// Pname, Lname, and Cname
+	// are the valid identifiers
+	var Pname = "   "
+	var Lname = "   "
+	var Cname = "   "
+
+	fmt.Printf("Portal name: %s", Pname)
+	fmt.Printf("\nLanguage name: %s", Lname)
+	fmt.Printf("\nChapter name: %s", Cname)
+
+}
